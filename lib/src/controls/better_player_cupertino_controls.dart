81b490f61efa5c0cd9a83040980fa6568ece7397
Jakub <jhomlala@gmail.com>
2020-04-30 23:08:43 +0200

    Added reusable video_player inside package

--- lib/src/controls/better_player_cupertino_controls.dart
+++ lib/src/controls/better_player_cupertino_controls.dart
@@ -7,8 +7,8 @@ import 'package:better_player/src/controls/better_player_cupertino_progress_bar.
 import 'package:better_player/src/controls/better_player_progress_colors.dart';
 import 'package:better_player/src/core/better_player_controller.dart';
 import 'package:better_player/src/core/utils.dart';
+import 'package:better_player/src/video_player/video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class BetterPlayerCupertinoControls extends StatefulWidget {
   ///Callback used to send information if player bar is hidden or not
@@ -245,7 +245,6 @@ class _BetterPlayerCupertinoControlsState
       ),
     );
   }
-
 
   GestureDetector _buildMuteButton(
     VideoPlayerController controller,
