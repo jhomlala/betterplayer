d020e586a9927b93dc1587e94a8cd463c4bd915b
Jakub  Homlala <jhomlala@gmail.com>
2020-11-07 22:46:50 +0100

    DA fixes

--- example/lib/other_page/other_page.dart
+++ example/lib/other_page/other_page.dart
@@ -1,6 +1,5 @@
 import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class OtherPage extends StatefulWidget {
   @override
