dce3917158ddb351ba2037308734a3cf3ef24089
jhomlala <jhomlala@gmail.com>
2021-02-28 13:06:13 +0100

    Added BP images

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
