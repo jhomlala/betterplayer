e27fdb0c71a794c67d5638c8dd1584029531e0bc
James Hancock <65730106+jhancock4d@users.noreply.github.com>
2021-09-22 13:50:16 -0400

    Delete example directory

--- example/lib/model/video_list_data.dart
+++
@@ -1,8 +0,0 @@
-class VideoListData {
-  final String videoTitle;
-  final String videoUrl;
-  Duration? lastPosition;
-  bool? wasPlaying = false;
-
-  VideoListData(this.videoTitle, this.videoUrl);
-}
--- example/lib/utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:path_provider/path_provider.dart';
-
-class Utils {
-  static Future<String> getFileUrl(String fileName) async {
-    final directory = await getApplicationDocumentsDirectory();
-    return "${directory.path}/$fileName";
-  }
-}
