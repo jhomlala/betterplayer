add9402a5d5f2fa3d75c0689bc1b553813f50e8c
Jakub <jhomlala@gmail.com>
2022-06-05 15:00:44 +0200

    Flutter 3.0 update

--- lib/src/core/better_player.dart
+++ lib/src/core/better_player.dart
@@ -63,7 +63,7 @@ class _BetterPlayerState extends State<BetterPlayer>
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addObserver(this);
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
@@ -110,7 +110,7 @@ class _BetterPlayerState extends State<BetterPlayer>
           _betterPlayerConfiguration.deviceOrientationsAfterFullScreen);
     }
 
-    WidgetsBinding.instance!.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     _controllerEventSubscription?.cancel();
     widget.controller.dispose();
     VisibilityDetectorController.instance
