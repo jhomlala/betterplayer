b4ffb8d75c0cfe55a1248b27f5ae96cfed452357
Huma Shaikh <huma.shaikh@monstar-lab.com>
2022-10-17 10:23:26 +0900

    intend

--- lib/src/video_player/video_player.dart
+++ lib/src/video_player/video_player.dart
@@ -169,13 +169,11 @@ class VideoPlayerValue {
 class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   final BetterPlayerBufferingConfiguration bufferingConfiguration;
 
-
   /// Constructs a [VideoPlayerController] and creates video controller on platform side.
-  VideoPlayerController( {
-
+  VideoPlayerController({
     this.bufferingConfiguration = const BetterPlayerBufferingConfiguration(),
     bool autoCreate = true,
-  }) : super(VideoPlayerValue(duration:  null)) {
+  }) : super(VideoPlayerValue(duration: null)) {
     if (autoCreate) {
       _create();
     }
@@ -216,7 +214,6 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
       videoEventStreamController.add(event);
       switch (event.eventType) {
         case VideoEventType.initialized:
-
           value = value.copyWith(
             duration: event.duration,
             size: event.size,
