863e02463c5aef819fc8bad2bcaca7af880bea39
Hugo Bedford <36144175+hbedford@users.noreply.github.com>
2023-01-02 15:02:05 -0300

    correction of speed video (#1)

--- lib/src/core/better_player_controller.dart
+++ lib/src/core/better_player_controller.dart
@@ -1260,7 +1260,7 @@ class BetterPlayerController {
   ///cache started for given [betterPlayerDataSource] then it will be ignored.
   Future<void> stopPreCache(
       BetterPlayerDataSource betterPlayerDataSource) async {
-    return VideoPlayerController?.stopPreCache(betterPlayerDataSource.url,
+    return VideoPlayerController.stopPreCache(betterPlayerDataSource.url,
         betterPlayerDataSource.cacheConfiguration?.key);
   }
 
