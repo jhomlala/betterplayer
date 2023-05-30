15b1d8c57aee59cb437d88c3cb3c36cc3974f704
Thanh Luu D. VN. HN <thanh.luu@monstar-lab.com>
2023-05-30 14:41:09 +0700

    fix sometimes auto pip not work when changing video

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -696,12 +696,13 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 - (void)willStartPictureInPicture: (bool) willStart
 {
     self._willStartPictureInPicture = willStart;
+    _pipController = nil;
+    
     if (willStart) {
-        if(!_pipController) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)),
+                       dispatch_get_main_queue(), ^{
             [self preparePictureInPicture:CGRectZero];
-        }
-    } else {
-        _pipController = nil;
+        });
     }
 }
 
