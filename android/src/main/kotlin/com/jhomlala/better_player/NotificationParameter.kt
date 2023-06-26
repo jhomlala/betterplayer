c8c019e42e698065fe161cea877996745a3acf71
Hiroshi Oshiro <ooshiro010@gamil.com>
2023-06-26 22:23:49 +0900

    Use data class

--- android/src/main/kotlin/com/jhomlala/better_player/NotificationParameter.kt
+++ android/src/main/kotlin/com/jhomlala/better_player/NotificationParameter.kt
@@ -2,7 +2,7 @@ package com.jhomlala.better_player
 
 import android.support.v4.media.session.MediaSessionCompat
 
-class NotificationParameter(
+data class NotificationParameter(
     val title: String,
     val author: String,
     val imageUrl: String,
