5a08eacaea589dc5a51ede743ea7d041f3bea0c7
Michał Jabłoński <mjablonski@codespot.io>
2023-08-15 17:12:34 +0200

    ATV-417 removed wake lock which was not handled fully in library - moving this logic to app

--- lib/src/core/better_player.dart
+++ lib/src/core/better_player.dart
@@ -6,7 +6,6 @@ import 'package:better_player/src/core/better_player_with_controls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:visibility_detector/visibility_detector.dart';
-import 'package:wakelock/wakelock.dart';
 
 ///Widget which uses provided controller to render video player.
 class BetterPlayer extends StatefulWidget {
@@ -102,7 +101,6 @@ class _BetterPlayerState extends State<BetterPlayer>
     ///full screen is on, then full screen route must be pop and return to normal
     ///state.
     if (_isFullScreen) {
-      Wakelock.disable();
       _navigatorState.maybePop();
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
           overlays: _betterPlayerConfiguration.systemOverlaysAfterFullScreen);
@@ -243,17 +241,9 @@ class _BetterPlayerState extends State<BetterPlayer>
       );
     }
 
-    if (!_betterPlayerConfiguration.allowedScreenSleep) {
-      Wakelock.enable();
-    }
-
     await Navigator.of(context, rootNavigator: true).push(route);
     _isFullScreen = false;
     widget.controller.exitFullScreen();
-
-    // The wakelock plugins checks whether it needs to perform an action internally,
-    // so we do not need to check Wakelock.isEnabled.
-    Wakelock.disable();
 
     await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: _betterPlayerConfiguration.systemOverlaysAfterFullScreen);
