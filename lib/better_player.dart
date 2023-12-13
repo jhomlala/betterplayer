3426a0dca12e03881279028befa368ccdd5cfc12
Paul Fauchon <paulfauchon@xendit.co>
2023-12-13 14:25:52 +0800

    add useful export

--- lib/better_player.dart
+++ lib/better_player.dart
@@ -37,3 +37,4 @@ export 'src/subtitles/better_player_subtitles_configuration.dart';
 export 'src/subtitles/better_player_subtitles_source.dart';
 export 'src/subtitles/better_player_subtitles_source_type.dart';
 export 'src/video_player/video_player.dart' show VideoPlayerValue;
+export 'src/video_player/video_player_platform_interface.dart';
