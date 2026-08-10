de60164537e1b1a25a8c8055ca43a16cdf78079e
Jakub  Homlala <jhomlala@gmail.com>
2026-08-10 22:23:38 +0200

    Fix pub.dev score: Add iOS SPM support, modernize Android build, and update dependencies

---
+++ ios/better_player/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "better_player",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "better-player", targets: ["better_player_objc"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hyperoslo/Cache", from: "6.0.0"),
+        .package(url: "https://github.com/swisspol/GCDWebServer", branch: "master"),
+        .package(url: "https://github.com/StyleShare/HLSCachingReverseProxyServer", from: "0.2.0"),
+        .package(url: "https://github.com/pinterest/PINCache", from: "3.0.0")
+    ],
+    targets: [
+        .target(
+            name: "better_player",
+            dependencies: [
+                .product(name: "Cache", package: "Cache")
+            ],
+            path: "Sources/better_player_swift"
+        ),
+        .target(
+            name: "better_player_objc",
+            dependencies: [
+                "better_player",
+                .product(name: "GCDWebServer", package: "GCDWebServer"),
+                .product(name: "HLSCachingReverseProxyServer", package: "HLSCachingReverseProxyServer"),
+                .product(name: "PINCache", package: "PINCache")
+            ],
+            path: "Sources/better_player",
+            publicHeadersPath: "include"
+        )
+    ]
+)
