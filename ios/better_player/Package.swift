e51b6c2416e23438e1d7b329c10c193fc70d105e
Jakub  Homlala <jhomlala@gmail.com>
2026-08-21 18:10:09 +0200

    feat: align with official Flutter SPM support requirements

--- ios/better_player/Package.swift
+++ ios/better_player/Package.swift
@@ -4,12 +4,13 @@ import PackageDescription
 let package = Package(
     name: "better_player",
     platforms: [
-        .iOS(.v13)
+        .iOS("13.0")
     ],
     products: [
-        .library(name: "better-player", targets: ["better_player_objc"])
+        .library(name: "better-player", targets: ["better_player"])
     ],
     dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(url: "https://github.com/hyperoslo/Cache", from: "6.0.0"),
         .package(url: "https://github.com/yene/GCDWebServer", from: "3.5.7"),
         .package(url: "https://github.com/StyleShare/HLSCachingReverseProxyServer", from: "0.2.0"),
@@ -19,20 +20,16 @@ let package = Package(
         .target(
             name: "better_player",
             dependencies: [
-                .product(name: "Cache", package: "Cache")
-            ],
-            path: "better_player/Sources/better_player_swift"
-        ),
-        .target(
-            name: "better_player_objc",
-            dependencies: [
-                "better_player",
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
+                .product(name: "Cache", package: "Cache"),
                 .product(name: "GCDWebServer", package: "GCDWebServer"),
                 .product(name: "HLSCachingReverseProxyServer", package: "HLSCachingReverseProxyServer"),
                 .product(name: "PINCache", package: "PINCache")
             ],
-            path: "better_player/Sources/better_player",
-            publicHeadersPath: "include"
+            path: "Sources/better_player",
+            cSettings: [
+                .headerSearchPath("include/better_player")
+            ]
         )
     ]
 )
