2581601f54183d9256e33be3443300984785494d
Jakub  Homlala <jhomlala@gmail.com>
2026-08-27 21:39:02 +0200

    fix: replace hardcoded absolute import in generated ObjC file and swiftgen tool

--- packages/better_player_ios/ios/better_player_ios/Sources/better_player_ios_objc/better_player.m
+++ packages/better_player_ios/ios/better_player_ios/Sources/better_player_ios_objc/better_player.m
@@ -1,7 +1,11 @@
 #include <stdint.h>
 #import <Foundation/Foundation.h>
 #import <objc/message.h>
-#import "../../../../../../../../../../../var/folders/df/djsxfhc17x95674wsm_g8s980000gn/T/WAqNYm/better_player_ios.h"
+#if __has_include(<better_player_ios/better_player_ios-Swift.h>)
+#import <better_player_ios/better_player_ios-Swift.h>
+#else
+#import "better_player_ios-Swift.h"
+#endif
 
 #if !__has_feature(objc_arc)
 #error "This file must be compiled with ARC enabled"
