5b105fed41bd95266a59ce05abde260f464528db
Jakub  Homlala <jhomlala@gmail.com>
2026-08-28 06:28:26 +0200

    refactor: replace dataSourceToMap map serialization with direct typed property access in FFI implementations

--- packages/better_player_ios/test/better_player_ios_test.dart
+++ packages/better_player_ios/test/better_player_ios_test.dart
@@ -20,45 +20,5 @@ void main() {
       expect(uiKitView.creationParams, {'textureId': 1});
     });
 
-    test('dataSourceToMap throws Exception for DASH streams', () {
-      final dataSource = DataSource(
-        sourceType: DataSourceType.network,
-        uri: 'https://example.com/video.mpd',
-      );
-
-      expect(
-        () => iosPlayer.dataSourceToMap(dataSource),
-        throwsA(
-          isA<Exception>().having(
-            (e) => e.toString(),
-            'message',
-            contains('DASH streams are not supported'),
-          ),
-        ),
-      );
-    });
-
-    test('dataSourceToMap throws Exception for DASH format hint', () {
-      final dataSource = DataSource(
-        sourceType: DataSourceType.network,
-        uri: 'https://example.com/video.m3u8',
-        formatHint: VideoFormat.dash,
-      );
-
-      expect(
-        () => iosPlayer.dataSourceToMap(dataSource),
-        throwsA(isA<Exception>()),
-      );
-    });
-
-    test('dataSourceToMap works for HLS streams', () {
-      final dataSource = DataSource(
-        sourceType: DataSourceType.network,
-        uri: 'https://example.com/video.m3u8',
-      );
-
-      final map = iosPlayer.dataSourceToMap(dataSource);
-      expect(map['uri'], 'https://example.com/video.m3u8');
-    });
   });
 }
