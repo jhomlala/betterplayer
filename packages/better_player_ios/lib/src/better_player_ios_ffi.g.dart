c65048e78f0366ff9398c774636ab0f228842b5d
jhomlala <1329033+jhomlala@users.noreply.github.com>
2026-08-28 15:53:34 +0000

    chore: Generate Android and iOS FFI bindings

--- packages/better_player_ios/lib/src/better_player_ios_ffi.g.dart
+++ packages/better_player_ios/lib/src/better_player_ios_ffi.g.dart
@@ -4571,7 +4571,7 @@ final _objc_msgSend_xtuoz7 = objc.msgSendPointer
 external ffi.Pointer<objc.ObjCProtocolImpl>
 _protocol_BetterPlayerCallback_raw();
 final _protocol_BetterPlayerCallback = objc.getProtocol(
-  "BetterPlayerCallback",
+  "better_player_ios.BetterPlayerCallback",
   _protocol_BetterPlayerCallback_raw,
 );
 late final _sel__dummyCallback = objc.registerName("_dummyCallback");
