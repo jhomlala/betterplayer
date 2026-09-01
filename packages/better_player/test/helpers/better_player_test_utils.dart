a245fdd9c691faaf6ef5983107a1b5e9691cae57
Jakub  Homlala <jhomlala@gmail.com>
2026-09-01 22:21:26 +0200

    refactor: Update test utils for engine (Step 4)

--- packages/better_player/test/helpers/better_player_test_utils.dart
+++ packages/better_player/test/helpers/better_player_test_utils.dart
@@ -3,7 +3,7 @@ import 'package:better_player/src/engine/player_engine_controller.dart';
 
 import 'better_player_mock_controller.dart';
 import 'mock_better_player_platform.dart';
-import 'mock_video_player_controller.dart';
+import 'mock_player_engine_controller.dart';
 
 class BetterPlayerTestUtils {
   static const String bugBuckBunnyVideoUrl =
@@ -18,21 +18,19 @@ class BetterPlayerTestUtils {
   }
 
   static BetterPlayerMockController setupBetterPlayerMockController({
-    PlayerEngineController? controller,
+    PlayerEngineController? engine,
     PlayerConfiguration configuration = const PlayerConfiguration(),
   }) {
     final mockController = BetterPlayerMockController(configuration);
-    if (controller != null) {
-      mockController.videoPlayerController = controller;
+    if (engine != null) {
+      mockController.videoPlayerController = engine;
     }
     return mockController;
   }
 
-  static MockVideoPlayerController setupMockVideoPlayerControler() {
-    final mockVideoPlayerController = MockVideoPlayerController();
-    mockVideoPlayerController.setNetworkDataSource(
-      BetterPlayerTestUtils.forBiggerBlazesUrl,
-    );
-    return mockVideoPlayerController;
+  static MockPlayerEngineController setupMockVideoPlayerControler() {
+    final mock = MockPlayerEngineController();
+    mock.setNetworkDataSource(BetterPlayerTestUtils.forBiggerBlazesUrl);
+    return mock;
   }
 }
