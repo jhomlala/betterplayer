aabf0d6491b63f4a3fafe6c3732b9da6b71e4a77
Jakub  Homlala <jhomlala@gmail.com>
2026-09-07 18:53:50 +0200

    feat: automatically close app and exit job after web tests completion in CI

--- packages/better_player_web/test/all_tests.dart
+++ packages/better_player_web/test/all_tests.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'better_player_web_test.dart' as better_player_web_test;
 import 'web_video_player_test.dart' as web_video_player_test;
 
@@ -24,22 +25,18 @@ void main() {
 
   // We use a small delay to let the app settle
   Future.delayed(Duration(seconds: 3), () async {
-    print('--- Starting All Web Tests ---');
-    
     try {
       print('>>> Running better_player_web_test.main()');
-      // Note: better_player_web_test.main() uses flutter_test functions 
-      // which might throw if not run by 'flutter test'.
-      // If they are purely unit tests, they might work or we might need 
-      // to adapt them.
       better_player_web_test.main();
       
       print('>>> Running web_video_player_test.main()');
       web_video_player_test.main();
       
-      print('--- All Web Tests Finished ---');
+      tearDownAll(() {
+        print('=== WEB TESTS COMPLETED ===');
+      });
     } catch (e, stack) {
-      print('Error during test execution: $e');
+      print('Error during test registration: $e');
       print(stack);
     }
   });
