3ec512d497fe572256e7734242cc50d455938e16
Jakub  Homlala <jhomlala@gmail.com>
2026-09-09 22:19:31 +0200

    fix: resolve common issues

--- packages/better_player_ios/ios/better_player_ios/Sources/better_player_ios/BetterPlayer.swift
+++ packages/better_player_ios/ios/better_player_ios/Sources/better_player_ios/BetterPlayer.swift
@@ -32,6 +32,15 @@ public typealias FlutterResult = (Any?) -> Void
 public class BetterPlayerView: UIView {
     public var player: AVPlayer?
 }
+public enum BetterPlayerTimeUtils {
+    public static func cmTimeToMillis(_ time: CMTime) -> Int64 {
+        guard time.timescale != 0 else { return 0 }
+        return Int64(time.value) * 1000 / Int64(time.timescale)
+    }
+    public static func timeIntervalToMillis(_ interval: TimeInterval) -> Int64 {
+        return Int64(interval * 1000.0)
+    }
+}
 #endif
 import Foundation
 import UIKit
