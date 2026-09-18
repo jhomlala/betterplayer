a5342820a21c35963af031697d853c51fda3e019
Jakub  Homlala <jhomlala@gmail.com>
2026-09-18 14:28:47 +0200

    fix: format

--- packages/better_player/test/playlist/better_player_playlist_controller_test.dart
+++ packages/better_player/test/playlist/better_player_playlist_controller_test.dart
@@ -6,7 +6,7 @@ import '../helpers/better_player_test_utils.dart';
 void main() {
   test('changedPlaylistItem event is dispatched on setupDataSource', () async {
     BetterPlayerTestUtils.setupMockPlatform();
-    
+
     final dataSourceList = [
       PlayerDataSource.network('https://example.com/video1.mp4'),
       PlayerDataSource.network('https://example.com/video2.mp4'),
@@ -25,6 +25,9 @@ void main() {
     playlistController.setupDataSource(1);
 
     expect(events.isNotEmpty, isTrue);
-    expect(events.last.betterPlayerEventType, PlayerEventType.changedPlaylistItem);
+    expect(
+      events.last.betterPlayerEventType,
+      PlayerEventType.changedPlaylistItem,
+    );
   });
 }
